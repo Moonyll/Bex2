afee80627a476d5d91c32c143a086eb54004743d
Moonyll <s.pouwel@laposte.net>
2018-12-18 12:01:29 +0100

    Bex2

---
+++ Bex2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nombre;
+            for (nombre = 0; nombre < 10; nombre++)
+            {
+                Console.WriteLine("Bonjour, je suis le message N° "+ (nombre+1));
+            }
+        }
+    }
+}
