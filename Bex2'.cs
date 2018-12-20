b5b1342f2f86b49a0581bbb8035fb23b6a9a96d8
Moonyll <45941071+Moonyll@users.noreply.github.com>
2018-12-20 10:42:39 +0100

    Update Bex2'.cs

--- Bex2'.cs
+++ Bex2'.cs
@@ -10,10 +10,10 @@ namespace ConsoleApp26
     {
         static void Main(string[] args)
         {
-            int i = 0;
+            int i = 0; // ou mettre i=1
             while (i < 10)
             {
-                Console.WriteLine("Bonjour, je suis le message N°" + (i + 1));
+                Console.WriteLine("Bonjour, je suis le message N°" + (i + 1)); // enlever i+1
                 i++;
             }
         }
